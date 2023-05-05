cff46779b5e8515233d8f53a9998d307f0b70961
OisinWrites <oisin.banville@gmail.com>
2023-05-05 17:19:13 +0000

    add heroku tp allowed host

--- byte/settings.py
+++ byte/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['byte-restaurant-bookings-2.herokuapp.com']
 
 
 # Application definition
