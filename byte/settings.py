5846009e159e039f643a0d244a68c1d8f0a74bfe
OisinWrites <oisin.banville@gmail.com>
2023-05-09 09:44:03 +0000

    Update key and debug

--- byte/settings.py
+++ byte/settings.py
@@ -20,13 +20,13 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DEBUG = 'DEVELOPMENT' in os.environ
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
